6f4c436b875c9d4bb9aab34bbd7698aa4a192223
fabien.giannesini <fabien.giannesini@gmail.com>
2021-03-17 15:55:13 +0100

    No leap year if multiple of 100: red

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,18 @@ mod tests {
     use crate::is_leap_year;
 
     #[test]
-    fn should_be_a_leap_year() {
+    fn should_be_a_leap_year_if_multiple_of_4() {
         assert!(is_leap_year(1980))
     }
 
     #[test]
-    fn should_be_an_other_leap_year() {
+    fn should_be_an_other_leap_year_if_multiple_of_4() {
         assert!(is_leap_year(1984))
+    }
+
+    #[test]
+    fn should_not_be_a_leap_year_if_multiple_of_100() {
+        assert!(!is_leap_year(1900))
     }
 
     #[test]
